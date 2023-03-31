255702e2bca13e471ac4f32f46be78bbb508b1cd
CasGutHib <94913843+CasGutHib@users.noreply.github.com>
2023-03-31 11:41:39 +0200

    Weights and geometry 1

--- loading.py
+++ loading.py
@@ -5,7 +5,9 @@ import numpy as np
 cr = 2.57
 ct = 1.59
 c_mac = 2.11847
+b = 27.05
 MTOW = 23000
+sweep = 0
 
 W_w = 14.9 * MTOW
 W_wh = 1.8 * MTOW
@@ -17,4 +19,9 @@ W_ln = 0.5 * MTOW
 
 W_fusgroup = W_fus + W_lm + W_ln
 W_wgroup = W_w + W_wh + W_wv + W_en
+taper = ct/cr
+y_lemac = (b/6) * ((1 + 2*taper)/taper)
+print(y_lemac)
+x_lemac = y_lemac * tan(sweep)
+
 
